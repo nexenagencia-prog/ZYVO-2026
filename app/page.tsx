e70460e6f1e4c77f440aa43b2acaa214a732c8ab
nexenagencia-prog <nexenagencia@gmail.com>
2026-09-09 00:08:35 -0300

    perf: stop forcing dynamic home render

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import './home-overrides.css';
 import HomeClient from './HomeClient';
 import { loadHomeContent } from '../lib/cms/repository';
 
-export const dynamic = 'force-dynamic';
+export const revalidate = 3600;
 
 export default async function Home(){
   const content=await loadHomeContent();
