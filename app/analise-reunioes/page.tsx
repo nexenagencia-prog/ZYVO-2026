2c0719cad8dab94ab0561ed0d4cd0bd5fe858fc8
nexenagencia-prog <nexenagencia@gmail.com>
2026-09-11 12:23:08 -0300

    fix: remove duplicate analysis opener causing page lock

--- app/analise-reunioes/page.tsx
+++ app/analise-reunioes/page.tsx
@@ -1,6 +1,5 @@
 import SkillsPage from '../skills/page';
-import OpenAnalysisOnMount from './OpenAnalysisOnMount';
 
 export default function MeetingAnalysisPage(){
-  return <><SkillsPage/><OpenAnalysisOnMount/></>;
+  return <SkillsPage/>;
 }
